110ba8b8d204f71ae5062a2873564c57aa12b251
Wendel <wendelev01@gmail.com>
2024-10-21 23:55:14 -0300

    Criação de API de livraria

---
+++ server.js
@@ -0,0 +1,19 @@
+// import http from "http";
+import app from "./src/app.js";
+
+const PORT = 3000;
+
+// const rotas = {
+//     "/": "Curso de Express API",
+//     "/livros": "Entrei na rota livros",
+//     "/autores": "Entrei na rota autores"
+// };
+
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/plain"});
+//     res.end(rotas[req.url]);
+// });
+
+app.listen(PORT, () => {
+    console.log("Servidor escutando!");
+});
