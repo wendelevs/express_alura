eab869c48c09e519d8b444bd1eb2c42940c79ed2
Wendel <wendelev01@gmail.com>
2024-10-25 19:00:40 -0300

    Adição de rotas para a API

---
+++ src/controllers/livroController.js
@@ -0,0 +1,53 @@
+import livro from "../models/Livro.js";
+
+class LivroController {
+    static async listarLivros (req, res) {
+        try {
+            const listaLivros = await livro.find({});
+            res.status(200).json(listaLivros);
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - Falha na requisição` });
+        };
+    };
+
+    static async listarLivroPorId (req, res) {
+        try {
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id);
+            res.status(200).json(livroEncontrado);
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - Falha na requisição do livro` });
+        };
+    };
+
+    static async cadastrarLivro (req, res) {
+        try {
+            const novoLivro = await livro.create(req.body);
+            res.status(201).json({ message: "Criado com sucesso", livro: novoLivro });
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - Falha ao cadastrar livro` });
+        };
+    };
+
+    static async atualizarLivro (req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json({message: "Livro atualizado"});
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - Falha na atualização do livro` });
+        };
+    };
+
+    static async deletarLivro (req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id);
+            res.status(200).json({message: "Livro deletado"});
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - Falha na deleção do livro` });
+        };
+    };
+};
+
+export default LivroController;
